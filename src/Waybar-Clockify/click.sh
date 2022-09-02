fa74d430e155882326dd3b6646190c4c8c563054
kohane27 <kohane27@protonmail.com>
2022-09-02 12:53:05 +0800

    feat: allow tag active tracker again

--- src/Waybar-Clockify/click.sh
+++ src/Waybar-Clockify/click.sh
@@ -14,12 +14,21 @@
 # -hover-select -me-select-entry '' -me-accept-entry MousePrimary
 # utilize hover-select and accept an entry in a single click:
 
+selection=$(rofi -location 3 -dmenu -theme Arc-Dark.rasi -no-fixed-num-lines -hover-select -me-select-entry '' -me-accept-entry MousePrimary "$@" <~/.config/waybar/scripts/Waybar-Clockify/clockify-tags.txt)
+
 # no active tracker running; can start
 if [[ "$(timew get dom.active)" == 0 ]]; then
   # start
   timew start >/dev/null 2>&1
-  selection=$(rofi -location 3 -dmenu -theme Arc-Dark.rasi -no-fixed-num-lines -hover-select -me-select-entry '' -me-accept-entry MousePrimary "$@" <~/.config/waybar/scripts/Waybar-Clockify/clockify-tags.txt)
   # tag selection
   timew tag @1 "$selection"
-  exit 0
+# active tracker with tag
+elif [[ "$(timew get dom.active)" == 1 ]] && [[ "$(timew get dom.active.tag.count)" == 1 ]]; then
+  current_tag="$(timew get dom.active.tag.1)"
+  # untag current tag
+  timew untag @1 "$current_tag"
+  # tag again with new selection
+  timew tag @1 "$selection"
 fi
+
+exit 0
